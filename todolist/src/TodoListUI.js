d4107ee2ea8268c368e2faa4c53675a78cd10011
lidaomeng <2322754493@qq.com>
2020-01-31 21:55:57 +0800

    UI 容器组件

---
+++ todolist/src/TodoListUI.js
@@ -0,0 +1,36 @@
+import React, { Component} from "react";
+import { Input, Button, List } from 'antd';
+
+class TodoListUI extends Component{
+    render() {
+        return (
+            <div style={{marginTop: '10px', marginLeft: '10px'}}>
+                <div>
+                    <Input
+                        value={this.props.inputValue}
+                        placeholder={'todo info'}
+                        style={{width: '300px', marginRight: '10px'}}
+                        onChange={this.props.handleInputChange}
+                    />
+                    <Button type={"primary"} onClick={this.props.handleBtnClick}>提交</Button>
+                </div>
+                <List
+                    style={{marginTop: '10px', width: '300px'}}
+                    bordered
+                    dataSource={this.props.list}
+                    /*
+                    此处是难点，不好理解
+                     */
+                    renderItem={(item,index) => (
+                        <List.Item
+                            onClick={ (index) => {this.props.handleItemDelete(index)} }>
+                            {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+}
+
+export default TodoListUI;
