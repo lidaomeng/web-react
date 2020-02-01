8c355f8bff0ec98ffb6b9ee305ee8d53d344b928
lidaomeng <2322754493@qq.com>
2020-02-01 18:41:45 +0800

    tmp save

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -7,32 +7,52 @@ class TodoList extends Component{
             <div>
                 <div>
                     <input value={this.props.inputValue} onChange={this.props.changeInputValue}/>
-                    <button>提交</button>
+                    <button onClick={this.props.handleClick}>提交</button>
                 </div>
                 <ul>
-                    <li>list</li>
+                    {
+                        this.props.list.map((item, index) => {
+                            return <li key={index} onClick={this.props.handleItemDelete.bind(index)}> {item} </li>
+                        })
+                    }
                 </ul>
             </div>
         )
     }
-
-    handleInputChange(e) {
-        console.log(e.target.value)
-    }
 }
 
+/*
+属性
+ */
 const mapStateToProps = (state) => {
     return {
-        inputValue: state.inputValue
+        inputValue: state.inputValue,
+        list: state.list
     }
 };
 
+/*
+方法
+ */
 const mapDispatchToProps = (dispatch) => {
     return {
         changeInputValue(e) {
             const action = {
                 type: 'change_input_value',
                 value: e.target.value
+            };
+            dispatch(action);
+        },
+        handleClick() {
+            const action = {
+                type: 'add_list_item'
+            };
+            dispatch(action);
+        },
+        handleItemDelete(index) {
+            console.log('handleItemDelete',index);
+            const action = {
+                type: 'delete_list_item'
             };
             dispatch(action);
         }
