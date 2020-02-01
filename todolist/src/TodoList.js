1a81ac18311634519cea247f4503385450ab6529
lidaomeng <2322754493@qq.com>
2020-02-01 18:13:10 +0800

    react-redux

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,112 +1,42 @@
-import React, {Component, Fragment} from "react";
-import TodoItem from './TodoItem';
-import axios from 'axios';
+import React, { Component } from "react";
+import {connect} from 'react-redux';
 
-import './style.css';
-
-/*
-    纸上得来终觉浅，绝知此事要躬行。
- */
 class TodoList extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue: '',
-            list: []
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleBtnClick = this.handleBtnClick.bind(this);
-        this.handleItemDelete = this.handleItemDelete.bind(this);
-    }
-
     render() {
         return (
-            <Fragment>
-                {/*页面渲染*/}
+            <div>
                 <div>
-                    <label htmlFor="tips">请输入内容：</label>
-                    <input
-                        id={'tips'}
-                        className={'input'}
-                        value={this.state.inputValue}
-                        onChange={this.handleInputChange}
-                        ref={(fuck) => {this.fuck = fuck}}
-                    />
-                    <button onClick={this.handleBtnClick}>提交</button>
+                    <input value={this.props.inputValue} onChange={this.props.changeInputValue}/>
+                    <button>提交</button>
                 </div>
-                <ul ref={(ul) => {this.ul = ul}}>
-                    {this.getTodoItem()}
+                <ul>
+                    <li>list</li>
                 </ul>
-            </Fragment>
+            </div>
         )
     }
 
-    getTodoItem() {
-        return this.state.list.map((item, index) => {
-            return (
-                <TodoItem
-                    key={index}
-                    item={item}
-                    index={index}
-                    handleItemDelete={this.handleItemDelete}
-                />
-            )
-        });
-    }
-
     handleInputChange(e) {
-        // const value = e.target.value;
-        /*
-        ref可以直接操作Dom
-         */
-        const value = this.fuck.value;
-        // 对象
-        this.setState(() => (
-            {
-                inputValue: value
-            }
-        ));
-    }
-
-    handleBtnClick() {
-        this.setState((prevState) => (
-            {
-                list: [...prevState.list, prevState.inputValue],
-                inputValue: ''
-            }
-        ), () => {
-            /*
-            应该在回调函数中做统计
-             */
-            console.log(this.ul.querySelectorAll('div').length)
-        });
-        // console.log(this.ul.querySelectorAll('div').length)
-    }
-
-    // 函数
-    handleItemDelete(index) {
-        this.setState((prevState) => {
-            const list = [...prevState.list];
-            list.splice(index, 1);
-            return {
-                list
-            }
-        });
-    }
-
-    /*
-    在此处获取axios数据并展示
-     */
-    componentDidMount() {
-        axios.get('/api/todolist')
-            .then((res) => {
-                this.setState(() => ({
-                    list: [...res.data]
-                }));
-            })
-            .catch(() => {console.log('axios fail!')});
+        console.log(e.target.value)
     }
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value
+            };
+            dispatch(action);
+        }
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
